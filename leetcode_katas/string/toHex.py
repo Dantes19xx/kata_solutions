9fe9bd4d9ec6c3b7bafb612d2f55623b074f4852
Dmitriy Yakubovskiy <2dmiyak@gmail.com>
2025-09-26 12:53:00 +0500

    `

---
+++ leetcode_katas/string/toHex.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def toHex(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        
+        if num == 0:
+            return "0"
+        elif num < 0:
+            num = num % pow(2, 32) #num & 0xFFFFFFFF
+
+        hexal_digits = [10, 11, 12, 13, 14, 15]
+        base = 16
+        res = []
+
+
+        while num > 0:
+            curr = num % base
+            res.append(str(curr) if curr not in hexal_digits else chr(curr + 87))
+            num //= base
+
+        res.reverse()
+
+        return "".join(res)
+
+
+
+Solution().toHex(-1)
