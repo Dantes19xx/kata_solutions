384a7a5ac2374c93fa85345ce0ce118657f49290
Dmitriy Yakubovskiy <2dmiyak@gmail.com>
2025-09-27 11:21:31 +0500

    refactor hex func

--- leetcode_katas/string/toHex.py
+++ leetcode_katas/string/toHex.py
@@ -8,7 +8,10 @@ class Solution(object):
         if num == 0:
             return "0"
         elif num < 0:
-            num = num % pow(2, 32) #num & 0xFFFFFFFF
+            # num = num % pow(2, 32) #num & 0xFFFFFFFF
+            num = pow(2, 32) - abs(num)
+
+        # 4294967295 num = num % pow(2, 32)
 
         hexal_digits = [10, 11, 12, 13, 14, 15]
         base = 16
