218d0068f19cfec0fa0bb56d0c946a8eadaf4a67
Dmitriy Yakubovskiy <dev6@ico.kz>
2025-11-03 13:27:28 +0500

    `

---
+++ leetcode_katas/string/1071_gcdOfStrings.py
@@ -0,0 +1,26 @@
+from math import gcd
+
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        length = gcd(len(str1), len(str2))
+        return str1[:length]
+
+
+Solution().gcdOfStrings("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX")
+# 
+# Solution().gcdOfStrings("ABABABAB", "ABAB")
+
