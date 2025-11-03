9c134d4c4be1975daa8308aee389032446ff73d6
Dmitriy Yakubovskiy <2dmiyak@gmail.com>
2025-11-03 07:50:54 +0500

    `

---
+++ leetcode_katas/string/1768_mergeAlternately.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        
+        i = j = 0
+        res = ""
+
+        while i < len(word1) and j < len(word2):
+            res += word1[i]
+            res += word2[j]
+
+            i += 1
+            j += 1
+        
+        res+= word1[i:]
+        res+= word2[j:]
+
+        return res
+
+    
+Solution().mergeAlternately("abcd", "pq")
