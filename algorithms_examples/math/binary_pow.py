9f81559a300f8ad0270a75f347916af7688b6139
Dmitriy Yakubovskiy <dev6@ico.kz>
2025-11-04 15:38:55 +0500

    `

--- algorithms_examples/math/binary_pow.py
+++ algorithms_examples/math/binary_pow.py
@@ -1,11 +1,14 @@
-def bin_pow(a, n):
-    if n == 0:
+def bin_pow(self, a, x):
+    if x == 0:
         return 1
     
-    if n % 2 == 1:
-        return bin_pow(a, n - 1) * a
-
-    b = bin_pow(a, n // 2)
+    if x < 0:
+        return 1 / self.bin_pow(a, -x)
+    
+    if x % 2 == 1:
+        return self.bin_pow(a, x-1) * a
+    
+    b = self.bin_pow(a, x//2)
 
     return b * b
 
---
+++ leetcode_katas/math/50_myPow.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+
+        a = self.bin_pow(x, n)
+
+        return a
+    
+    def bin_pow(self, a, x):
+        if x == 0:
+            return 1
+        
+        if x < 0:
+            return 1 / self.bin_pow(a, -x)
+        
+        if x % 2 == 1:
+            return self.bin_pow(a, x-1) * a
+        
+        b = self.bin_pow(a, x//2)
+
+        return b * b
+    
+Solution().myPow(2.00000, -2)
